1398b14cfefe928713b2762eb3fa2251eb52d345
Pratik <71585947+Pratik543@users.noreply.github.com>
2025-10-17 15:55:53 +0530

    update aws-resource-list script

--- aws-resource-list.sh
+++ aws-resource-list.sh
@@ -66,7 +66,6 @@ if [ ! -d ~/.aws ]; then
     Default Output Format: json, yaml, table, text. 
     "
     aws configure
-    exit 1
 fi
 
 echo "Option for AWS regions: us-east-1, us-east-2, us-west-1, us-west-2, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, eu-central-1, eu-west-1, eu-west-2, sa-east-1"
