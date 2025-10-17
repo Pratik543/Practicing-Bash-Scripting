73c7cf3ebd171a625ecfa20e4d3f5734b8a92217
Pratik <71585947+Pratik543@users.noreply.github.com>
2025-10-17 16:15:59 +0530

    updated aws-resource-list script

--- aws-resource-list.sh
+++ aws-resource-list.sh
@@ -26,54 +26,25 @@
 # Example: ./aws-resource-list.sh
 ###############################################################
 
-# Update package lists and add dependencies
-sudo apt update -y 
+# Helper functions
+print_red() {
+  echo -e "\e[31m$1\e[0m"
+}
 
-sudo apt install unzip curl -y
+print_green() {
+  echo -e "\e[32m$1\e[0m"
+}
 
+print_white() {
+  echo -e "\e[37m$1\e[0m"
+}
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-
-# Check if the AWS CLI is installed
-if ! command -v aws &> /dev/null; then
-    echo "AWS CLI is not installed. Please install it and try again."
-
-    echo "-------------------------------------------------------"
-    echo "Running the following command to install AWS CLI:"
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
-    aws --version
-else
-    echo "AWS CLI is installed."
-fi
-
-# Check if the AWS CLI is configured
-if [ ! -d ~/.aws ]; then
-    echo "AWS CLI is not configured. Please configure it and try again."    
-
-    echo "-------------------------------------------------------"
-    echo "Running the following command to configure AWS CLI:"
-    echo "aws configure
-    then provide the following details:
-    
-    AWS Access Key ID: your_access_key_id
-    AWS Secret Access Key: your_secret_access_key
-    Default Region: us-east-1, us-east-2, us-west-1, us-west-2, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, eu-central-1, eu-west-1, eu-west-2, sa-east-1.
-    Default Output Format: json, yaml, table, text. 
-    "
-    aws configure
-fi
-
-echo "Option for AWS regions: us-east-1, us-east-2, us-west-1, us-west-2, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, eu-central-1, eu-west-1, eu-west-2, sa-east-1"
-echo "-------------------------------------------------------"
+print_green "Option for AWS regions: us-east-1, us-east-2, us-west-1, us-west-2, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, eu-central-1, eu-west-1, eu-west-2, sa-east-1"
+print_white "-------------------------------------------------------"
 read -p "Enter the AWS region: " aws_region
 
-echo "Option for AWS services: ec2, s3, iam, rds, lambda, ebs, elb, sqs, sns, kms, cloudfront, cloudwatch, cloudtrail, route53, cloudformation"
-echo "-------------------------------------------------------"
+print_green "Option for AWS services: ec2, s3, iam, rds, lambda, ebs, elb, sqs, sns, kms, cloudfront, cloudwatch, cloudtrail, route53, cloudformation"
+print_white "-------------------------------------------------------"
 read -p "Enter the AWS service name: " aws_service
 
 case $aws_service in
@@ -123,6 +94,6 @@ case $aws_service in
         aws cloudformation list-stacks --region $aws_region
         ;;
     *)
-        echo "Invalid service name. Please choose from the following: ec2, s3, iam, rds, lambda, ebs, elb, sqs, sns, kms, cloudfront, cloudwatch, cloudtrail, route53, cloudformation"
+        print_red "Invalid service name. Please choose from the following: ec2, s3, iam, rds, lambda, ebs, elb, sqs, sns, kms, cloudfront, cloudwatch, cloudtrail, route53, cloudformation"
         ;;
 esac
